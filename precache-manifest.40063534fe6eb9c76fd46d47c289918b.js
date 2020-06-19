31a619846324207e06d52e00e9b862e197f3bb44
ahmaadidrees <ahmaadidrees@gmail.com>
2020-06-19 15:58:27 -0700

    Updates

---
+++ precache-manifest.40063534fe6eb9c76fd46d47c289918b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4d1dec3b0789fdf99cf6fcd958e65c",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "adf3f011096dc2c2f691",
+    "url": "/React-ToDo/static/css/main.5b94ed3b.chunk.css"
+  },
+  {
+    "revision": "2620b3b9e301aae2a4d2",
+    "url": "/React-ToDo/static/js/2.e8cdaa83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo/static/js/2.e8cdaa83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf3f011096dc2c2f691",
+    "url": "/React-ToDo/static/js/main.97607460.chunk.js"
+  },
+  {
+    "revision": "1b8b6db901282d37024e",
+    "url": "/React-ToDo/static/js/runtime-main.8e832e66.js"
+  }
+]);
